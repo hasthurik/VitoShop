32eb906344b3e4f4d9bd3a821179e32f1c00e791
Vito <vkubik09@mail.ru>
2025-04-05 19:46:44 +0300

    0.1

---
+++ src/main/java/com/example/VitoShop/VitoShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.VitoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitoShopApplication.class, args);
+	}
+
+}
